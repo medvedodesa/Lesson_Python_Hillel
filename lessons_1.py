14b1ccaabee704902d30281a127ebba396d26dc6
TimiRout <timi.rout@gmail.com>
2020-09-03 20:08:17 +0300

    my list

--- lessons_1.py
+++ lessons_1.py
@@ -3,6 +3,7 @@
 num1 = float(input('Please, enter first number: '))
 num2 = float(input('Please, enter second number: '))
 num3 = float(input('Please, enter third number: '))
+
 # We perform operation of adding numbers:
 
 num = num1 + num2 + num3
