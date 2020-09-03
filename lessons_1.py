7a4a7c6f16a291d5b650e1b6dbce71f1e7db2c64
TimiRout <timi.rout@gmail.com>
2020-09-03 19:05:28 +0300

    For GitHub

---
+++ lessons_1.py
@@ -0,0 +1,12 @@
+# Advanced calculator
+
+num1 = int(input('Please, enter first number: '))
+num2 = int(input('Please, enter second number: '))
+
+# We perform operation of adding numbers:
+
+num = num1 + num2
+
+# Output on display
+
+print("Result is amounts number: " + str(num))
