0c177d4ae93869c37646211900eb3698ef3a8809
TimiRout <timi.rout@gmail.com>
2020-09-03 20:02:16 +0300

    For GitHub

--- lessons_1.py
+++ lessons_1.py
@@ -1,11 +1,11 @@
 # Advanced calculator
 
-num1 = int(input('Please, enter first number: '))
-num2 = int(input('Please, enter second number: '))
-
+num1 = float(input('Please, enter first number: '))
+num2 = float(input('Please, enter second number: '))
+num3 = float(input('Please, enter third number: '))
 # We perform operation of adding numbers:
 
-num = num1 + num2
+num = num1 + num2 + num3
 
 # Output on display
 
