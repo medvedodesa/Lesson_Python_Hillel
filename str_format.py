f64b6f47c4d4d1599c9882ee505129b00978c9ce
TimiRout <timi.rout@gmail.com>
2020-09-04 02:44:44 +0300

    For GitHub

---
+++ str_format.py
@@ -0,0 +1,28 @@
+age = 23
+name = 'Кирилл'
+
+print('My name is  {0}.'.format(name))
+print('{0}у {1} age.'.format(name, age))
+
+print('{0:.^11}'.format('hello'))
+
+a = 5
+print(a)
+a += 1
+print(a)
+
+s = 'тут я напишу \
+всё что я хочу'
+print(s)
+
+a = 5
+b = 10
+
+aa = a
+bb = b
+
+a = bb
+b = aa
+
+print('he is \b``work')
+
