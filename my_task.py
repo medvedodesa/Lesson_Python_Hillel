9499b6a77fbc410b3fab6508c3d50ed13cb4608b
TimiRout <timi.rout@gmail.com>
2020-09-28 19:42:39 +0300

    For GitHub

--- my_task.py
+++ my_task.py
@@ -1,18 +1,54 @@
-dictionary = {}
+# !!!СОРТИРОВКА ПУЗЫРЬКОМ!!!
 
-num_str = int(input('Введите число строк: '))
+# from random import randint
+#
+# lst = [randint(0, 50) for _ in range(10)]
+#
+# print(lst)
+#
+# for i in range(len(lst)):
+#     for j in range(len(lst) - i - 1):
+#         if lst[j] > lst[j + 1]:
+#             lst[j], lst[j + 1] = lst[j + 1], lst[j]
+#             print(lst)
+#
+# swaps = True
+#
+# while swaps:
+#     swaps = False
+#     for j in range(len(lst) - 1):
+#         if lst[j] > lst[j + 1]:
+#             swaps = True
+#             lst[j], lst[j + 1] = lst[j + 1], lst[j]
+#
+# print(lst)
 
-for text in range(num_str):
-    string = input('Введите строку: ', )
-    for word in string:
-        dictionary[word] = dictionary.get(word, 0) + 1
 
-# Сперва мы находим максимальное значение нашего словаря
-max_value = max(dictionary.values())
+# !!! ФАКТОРИАЛ ЧИСЛА!!!
 
-# Через цикл пребираем все значения и если есть одинаковые значения выведит последнее.
-for key, values in dictionary.items():
-    if values == max_value:
-        word = key
+# num = int(input('Please enter else number: '))
+# k = 1
+#
+# for i in range(1, num + 1):
+#     k *= i
+#
+# print(k)
 
-print(dictionary[word])
+# num = int(input('Please enter else number: '))
+#
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     f = 1
+#     i = 1
+#
+#     while i <= n:
+#         f *= i
+#         i += 1
+#
+#     return f
+#
+# print(factorial(num))
+
+
+
