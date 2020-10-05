180a30b000da182bf66a45c0c70a71eaabe8b765
TimiRout <timi.rout@gmail.com>
2020-10-05 13:56:43 +0300

    For GitHub

--- my_task.py
+++ my_task.py
@@ -50,3 +50,55 @@
 #
 # print(factorial(num))
 
+from random import randint
+
+
+# def binary_search_2(array, key_value, left=0, right=None):
+#     if right is None:
+#         right = len(array)
+#
+#     j = 0
+#     middle = (left + right) // 2
+#     while array[middle] != key_value and left <= right:
+#         if array[middle] < key_value:
+#             left = middle + 1
+#             j += 1
+#         else:
+#             right = middle - 1
+#             j += 1
+#
+#         middle = (left + right) // 2
+#     return (True, j, middle, array[middle]) if not (left > right) else (False, j)
+
+
+# def binary_alg(args, key_value, low=0, high=None):
+#     if high is None:
+#         high = len(args)
+#
+#     mid = (low + high) // 2
+#     j = 0
+#     while args[mid] != key_value and low <= high:
+#         if args[mid] < key_value:
+#             low = mid + 1
+#             j += 1
+#         else:
+#             high = mid - 1
+#             j += 1
+#
+#         mid = (low + high) // 2
+#
+#     return (True, j, mid, args[mid]) if not (low > high) else (False, j)
+#
+#
+# lst = [randint(1, 2000) for _ in range(1000)]
+# print(lst)
+# print()
+# lst.sort()
+# print(lst)
+# print(len(lst))
+#
+# key = int(input('Введите любое число: '))
+# print()
+# print(binary_alg(lst, key))
+
+print(55 % 36)
